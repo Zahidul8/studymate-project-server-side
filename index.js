68464cbe9e4d5f6e3b0aab33c9e7f881d4225f19
Your NameZahidul8 <www.zahidul8@gmail.com>
2025-11-10 12:38:20 +0600

    add delete api for delete particular  partner request

--- index.js
+++ index.js
@@ -105,6 +105,14 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/partnerCount/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await partnerCountCollection.deleteOne(query);
+      res.send(result);
+
+    })
+
 
 
 
